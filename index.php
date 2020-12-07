eab2c1d90b3aad4df30d74195b3d81d8cf2d3658
Mit Krivich <mit@krivoshchekov.ru>
2020-12-07 15:44:31 +0300

    1

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
 $authToken='Test-4247c-3fc638-4b18-a01ed-1264T';
-
+$login='08022019@aliexpress-op.ru';
+$gmdate=gmdate('dmYH', time());
+$url='https://alix.brand.company/api_top3/';
+//{хэш md5} — Хэш md5 строки {login}{время GMT}{alix_token}
+$in=$login.$gmdate.$login;
+$hash=md5($in);
+echo "<P> In=".$in;
+echo "<P> Hash=".$hash;
+$authToken=$login.':'.$hash;
 $context = stream_context_create(array(
     'http' => array(
         'method' => 'POST',
         'header' => "Access-Token: {$authToken}\r\n".
-            "Content-Type: application/json\r\n",
-        'content' => json_encode($postData)
+                    "Content-Type: multipart/form-data\r\n".
+                    "Accept: application/json;charset=UTF-8\r\n",
+        'content' => 'content',
+
+
     )
 ));
+$response = trim(file_get_contents($url, false, $context));
+echo "Ответ:<BR>".$response;
+
